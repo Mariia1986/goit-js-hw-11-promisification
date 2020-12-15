0f841a7ce978d5a7a6aba1d9c1cd3df459a6bfda
Mariia1986 <renij1986@gmail.com>
2020-12-15 15:12:54 +0200

    добавлено 2 е задание

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const delay = ms => {
-     new Promise(resolve => setTimeout(() => {
-        resolve(logger(ms));
-      }, ms))
+// // const delay = ms => {
+// //      new Promise(resolve => setTimeout(() => {
+// //         resolve(logger(ms));
+// //       }, ms))
+// //   };
+  
+// //   const logger = time => console.log(`Resolved after ${time}ms`);
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+// ======================================================================
+
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+  const toggleUserState = (allUsers, userName, callback) => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+  
+    callback(updatedUsers);
   };
   
-  const logger = time => console.log(`Resolved after ${time}ms`);
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+  const logger = updatedUsers => console.table(updatedUsers);
+  
+  /*
+   * Сейчас работает так
+   */
+  toggleUserState(users, 'Mango', logger);
+  toggleUserState(users, 'Lux', logger);
+  
+  /*
+   * Должно работать так
+   */
+//   toggleUserState(users, 'Mango').then(logger);
+//   toggleUserState(users, 'Lux').then(logger);
