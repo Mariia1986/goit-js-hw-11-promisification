bba10bfef1f3114f8267a7680a1bec91aaa4d211
Mariia1986 <renij1986@gmail.com>
2020-12-17 10:47:50 +0200

    задание 2 гготово

--- index.js
+++ index.js
@@ -1,48 +1,33 @@
-const delay = ms => {
-    return new Promise(resolve => setTimeout(() => {
-        resolve(ms);
-      }, ms))
-  };
-  
-  const logger = time => console.log(`Resolved after ${time}ms`);
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+// const delay = ms => {
+//     return new Promise(resolve => setTimeout(() => {
+//         resolve(ms);
+//       }, ms))
+//   };
+
+//   const logger = time => console.log(`Resolved after ${time}ms`);
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
 // ======================================================================
 
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
 
-// const users = [
-//     { name: 'Mango', active: true },
-//     { name: 'Poly', active: false },
-//     { name: 'Ajax', active: true },
-//     { name: 'Lux', active: false },
-//   ];
-  
-//   const toggleUserState = (allUsers, userName) => {
-    
-//     const promise= new Promise((res,rej)=>{
-//       setTimeout(()=>{
-//         res()
-//       })
-//     })
-//     const updatedUsers = allUsers.map(user =>
-        
-//       user.name === userName ? { ...user, active: !user.active } : user,
-//     );
-  
-//     callback(updatedUsers);
-//   };
-  
-  // const logger = updatedUsers => console.table(updatedUsers);
-  
-  /*
-   * Сейчас работает так
-   */
-  // toggleUserState(users, 'Mango', logger);
-  // toggleUserState(users, 'Lux', logger);
-  
-  /*
-   * Должно работать так
-   */
-//   toggleUserState(users, 'Mango').then(logger);
-//   toggleUserState(users, 'Lux').then(logger);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+
+    res(updatedUsers);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
